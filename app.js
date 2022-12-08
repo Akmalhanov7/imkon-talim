5e6e3cdf79cb553b449c82afae1476be70f45b0e
akmalhanov7 <akmalhanovabdulbosit7@gmail.com>
2022-12-08 18:57:41 +0500

    imkon-talim

---
+++ app.js
@@ -0,0 +1,12 @@
+// loader js
+window.addEventListener('DOMContentLoaded' , () =>{
+    const loader = document.querySelector('.loader')
+    
+    setTimeout(() =>{
+        loader.style.opacity = 0
+        setTimeout(() =>{
+            loader.style.display = 'none'
+
+        }, 500)
+    }, 3000)
+})
